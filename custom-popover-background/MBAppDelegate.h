30ac7aa0f16d5418a0a59597218220b19ce0a193
i4niac <i4niac@i4napps.com>
2012-02-08 15:21:41 +1100

    Initial Commit

---
+++ custom-popover-background/MBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBAppDelegate.h
+//  custom-popover-background
+//
+//  Created by Grebenets Maksym on 8/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ custom-popover-background/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  custom-popover-background
+//
+//  Created by Grebenets Maksym on 8/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
